6beadbb7918d048dd1f4fb75ec16faf15a346250
Pragyan Kaushiley <39689733+PragyanKaushiley@users.noreply.github.com>
2019-05-15 17:46:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -50,6 +50,8 @@ app.post('/', function(req, res) {
   });
 })
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!')
-})
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+});
